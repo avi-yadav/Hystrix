45e0dd8bd9aefac122d16bd8eb6d36ebead228c4
Gautham Srinivas <gautham.srinivas@flipkart.com>
2016-01-05 13:15:21 +0530

    Uncommenting guice module

--- hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/aop/guice/InterceptorModule.java
+++ hystrix-contrib/hystrix-javanica/src/main/java/com/netflix/hystrix/contrib/javanica/aop/guice/InterceptorModule.java
@@ -1,24 +1,28 @@
 package com.netflix.hystrix.contrib.javanica.aop.guice;
 
+import com.google.inject.AbstractModule;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCollapser;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.cache.annotation.CacheRemove;
 
+import static com.google.inject.matcher.Matchers.annotatedWith;
+import static com.google.inject.matcher.Matchers.any;
+
 /**
  * Created by gautham.srinivas on 18/11/15.
  */
-public class InterceptorModule {//} extends AbstractModule{
-//    @Override
-//    protected void configure() {
-//
-//        bindInterceptor(
-//                any(), annotatedWith(HystrixCommand.class), new GuiceCommandInterceptor()
-//        );
-//        bindInterceptor(
-//                any(), annotatedWith(HystrixCollapser.class), new GuiceCommandInterceptor()
-//        );
-//        bindInterceptor(
-//                any(), annotatedWith(CacheRemove.class), new GuiceCacheInterceptor()
-//        );
-//    }
+public class InterceptorModule extends AbstractModule{
+    @Override
+    protected void configure() {
+
+        bindInterceptor(
+                any(), annotatedWith(HystrixCommand.class), new GuiceCommandInterceptor()
+        );
+        bindInterceptor(
+                any(), annotatedWith(HystrixCollapser.class), new GuiceCommandInterceptor()
+        );
+        bindInterceptor(
+                any(), annotatedWith(CacheRemove.class), new GuiceCacheInterceptor()
+        );
+    }
 }
